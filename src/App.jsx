4823c9f31efcbbad93907e9c5e74c6d346ca04e7
Ayas Mahmud <ayasmahmud48@gmail.com>
2025-10-08 13:14:45 +0600

    the assignment is begin

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { FaHome } from "react-icons/fa";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>hello   <FaHome /></div>
+      <div>hello world</div>
+      <p></p>
+
+    </>
+  )
+}
+
+export default App
